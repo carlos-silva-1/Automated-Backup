6457a311f5680720dd34b9e592a776d715f70df1
Carlos Silva <134824320+carlos-silva-1@users.noreply.github.com>
2024-02-14 20:02:42 -0300

    Added function to check if a file already exists on the drive and made changes to improve readability
    Used another function to check for existence (get_file_id), but it made the code harder to read.
    
    Added an alert function (alert) to alert the user that the folder in the drive does not exist.

--- backup_drive.py
+++ backup_drive.py
@@ -4,13 +4,21 @@ from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 from config import BACKUP_FOLDER_PATH, CREDS_PATH, SCOPES, DRIVE_FOLDER_NAME
 from google.oauth2 import service_account
+import notify2
 
 def fetch_oauth_credentials():
     return service_account.Credentials.from_service_account_file(CREDS_PATH)
 
-def folder_exists(drive_service, folder_name):
+def folder_exists_on_drive(drive_service, folder_name):
     folder_id = get_folder_id(drive_service, folder_name)
     if folder_id is None:
+        return False
+    else:
+        return True
+
+def file_exists_on_drive(drive_service, file_name):
+    file_id = get_file_id(drive_service, file_name)
+    if file_id is None:
         return False
     else:
         return True
@@ -42,42 +50,46 @@ def create_file_on_drive(drive_service, filename, folder_id, media):
         'name': filename,
         'parents': [folder_id]
     }
-    drive_service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+    drive_service.files().create(body=file_metadata, media_body=media).execute()
 
-def create_dir_on_drive(drive_service, dir_name, parent_dir_id):
+def create_folder_on_drive(drive_service, folder_name, parent_folder_id):
     folder_metadata = {
-        'name': dir_name,
+        'name': folder_name,
         'mimeType': 'application/vnd.google-apps.folder',
-        'parents': [parent_dir_id]
+        'parents': [parent_folder_id]
     }
-    folder = drive_service.files().create(body=folder_metadata, fields='id').execute()
+    drive_service.files().create(body=folder_metadata).execute()
 
 def upload_file(drive_service, filename, drive_folder_id, path):
     media = MediaFileUpload(os.path.join(path, filename), resumable=True)
-    existing_file_id = get_file_id(drive_service, filename)
-    if existing_file_id:
+    if file_exists_on_drive(drive_service, filename):
+        existing_file_id = get_file_id(drive_service, filename)
         update_file_on_drive(drive_service, existing_file_id, media)
     else:
         create_file_on_drive(drive_service, filename, drive_folder_id, media)
 
-# when passing args to a lower level in the recursion, do you always pass drive_folder_id?
-# or should it be the id of some other folder?
+# Uploads files in subfolders through recursion
 def upload_files_in_folder(drive_service, drive_folder_id, folder_path):
     for entry in os.listdir(folder_path):
         if os.path.isdir(os.path.join(folder_path, entry)):
-            if not folder_exists(drive_service, entry):
-                create_dir_on_drive(drive_service, entry, drive_folder_id)
+            if not folder_exists_on_drive(drive_service, entry):
+                create_folder_on_drive(drive_service, entry, drive_folder_id)
             subfolder_id = get_folder_id(drive_service, entry)
             upload_files_in_folder(drive_service, subfolder_id, os.path.join(folder_path, entry))
         else:
             upload_file(drive_service, entry, drive_folder_id, folder_path)
 
+# Creates an os notification
+def alert(message):
+    notify2.init("Alert")
+    notification = notify2.Notification("Alert", message)
+    notification.show()
+
 # Performs the backup of the local folder located at backup_folder_path to the 
 # folder named drive_folder_name to Google Drive
 def backup_to_folder_in_drive(drive_service, drive_folder_name, backup_folder_path):
-    if not folder_exists(drive_service, drive_folder_name):
-        pass
-        # CHANGE FROM CREATING FOLDER TO LAUNCHING AN ALERT
+    if not folder_exists_on_drive(drive_service, drive_folder_name):
+        alert('Error: the destination folder does not exist on the drive')
     else:
         drive_folder_id = get_folder_id(drive_service, drive_folder_name)
         upload_files_in_folder(drive_service, drive_folder_id, backup_folder_path)
