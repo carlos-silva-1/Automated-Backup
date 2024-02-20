c7bfcc07b194729bca891c7522ea6960290f5344
Carlos Silva <134824320+carlos-silva-1@users.noreply.github.com>
2024-02-20 10:43:49 -0300

    Delete config_example.py

--- config_example.py
+++
@@ -1,4 +0,0 @@
-BACKUP_FOLDER_PATH = '/path/to/local/folder/'
-CREDS_PATH = '/path/to/service-account/credentials/file/credential_file.json'
-SCOPES = ['https://www.googleapis.com/auth/drive']
-DRIVE_FOLDER_NAME = 'Folder_name_on_google_drive'
